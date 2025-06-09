8480c9cec189cc0590f51faf53f06217491435de
nagyzoltan1025 <36644322+nagyzoltan1025@users.noreply.github.com>
2025-06-09 13:49:59 +0200

    Exported the project to WebStorm. Created css files for the Header and CoreConcepts components and move the corresponding styles there.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <CoreConcepts
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcepts {...CORE_CONCEPTS[1]} />
+          <CoreConcepts {...CORE_CONCEPTS[2]} />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
